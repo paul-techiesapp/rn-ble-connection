f0f5b2f3a8939bbcb19590173e2fdd6e22e05823
Paul Lee <paul@techiesapp.io>
2022-05-19 15:00:39 +0800

    updated code

--- ios/bleTestTests/bleTestTests.m
+++ ios/bleTestTests/bleTestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface zensoTests : XCTestCase
+@interface bleTestTests : XCTestCase
 
 @end
 
-@implementation zensoTests
+@implementation bleTestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
